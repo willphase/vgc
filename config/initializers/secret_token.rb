df459c2437da7f15c7513d647ea68ca89f1e23ed
willphase <wherrington@gmail.com>
2014-05-20 19:48:12 -0400

    rails new done

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vgc::Application.config.secret_key_base = '3e9749649469c9ca6b14fd9d3928b9d3f9323c1dd694bef05e2ac5b5cf1b8c76de67926bd0e6795b2f08a9a5e55151125bb9cb992d6c8afb8a90420fccb5873f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Vgc::Application.config.session_store :cookie_store, key: '_vgc_session'
